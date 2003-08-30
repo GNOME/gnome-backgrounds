974103fe848ac66c6e02219add52c69935d2390d
Mark Finlay <sisob@src.gnome.org>
2003-08-30 08:26:05 +0000

    Initial revision

---
+++ autogen.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="gnome-backgrounds"
+
+(test -f $srcdir/configure.in \
+  && test -f $srcdir/index.theme.in \
+  && test -d $srcdir/48x48) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+echo "Running glib-gettextize"
+glib-gettextize --copy --force
+
+echo "Running intltoolize"
+intltoolize --copy --force --automake
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
