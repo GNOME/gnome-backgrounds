19df56a9f413a58d3c0e7e5aa3afcfecd07c1ecb
Javier Jardón <jjardon@gnome.org>
2010-10-04 16:53:35 +0200

    autogen.sh: configure.in -> configure.ac

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="gnome-backgrounds"
 
-(test -f $srcdir/configure.in \
+(test -f $srcdir/configure.ac \
   && test -d $srcdir/abstract ) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
